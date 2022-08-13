911bd5cef3f449c1995ca61a1b4a7d80246401c9
IShahnawazShaikh <shahnawazexplore@gmail.com>
2022-08-14 01:09:19 +0530

    OneToMany Bidirectional relationship wihtout extra table.

--- src/main/java/com/practice/entityrelationship/answer/AnswerEntity.java
+++ src/main/java/com/practice/entityrelationship/answer/AnswerEntity.java
@@ -16,8 +16,8 @@ public class AnswerEntity {
 
     @Column(name="answer")
     private String answer;
-//
-//    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
-//    @JoinColumn(name="question_id")
-//    QuestionEntity question;
+
+    @ManyToOne(fetch = FetchType.EAGER,cascade = CascadeType.ALL)
+    @JoinColumn(name="question_id")
+    QuestionEntity question;
 }
